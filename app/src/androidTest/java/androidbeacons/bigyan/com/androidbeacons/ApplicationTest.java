942302bad33785b427e52c21315259eb4d091da3
Bigyan Thapa <bigyan_4424@yahoo.com>
2015-12-29 09:08:50 -0600

    inital commit

---
+++ app/src/androidTest/java/androidbeacons/bigyan/com/androidbeacons/ApplicationTest.java
@@ -0,0 +1,13 @@
+package androidbeacons.bigyan.com.androidbeacons;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
